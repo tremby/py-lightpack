a47e52cd5dbe1898610ad8137081ab358d9b341b
Bart Nagel <bart@tremby.net>
2019-02-18 15:54:57 -0800

    Add full description to built package

--- setup.py
+++ setup.py
@@ -3,9 +3,14 @@
 from setuptools import setup
 import lightpack
 
+with open('README.md', 'r') as fh:
+	long_description = fh.read()
+
 setup(name=lightpack.NAME,
 		version=lightpack.VERSION,
 		description=lightpack.DESCRIPTION,
+		long_description=long_description,
+		long_description_content_type='text/markdown',
 		author=lightpack.AUTHOR,
 		author_email=lightpack.AUTHOR_EMAIL,
 		url=lightpack.URL,
